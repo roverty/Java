f4d931e4d4f80e09004b3b7b08fe54c39cfb736d
rhodstar <rhodfra@gmail.com>
2021-07-22 07:07:02 -0500

    Added some more examples of using threads in java

--- 05-programacion-multihilo/Ejemplo05Join.java
+++ 05-programacion-multihilo/Ejemplo05Join.java
@@ -37,7 +37,7 @@ class ThreadDemo extends Thread {
    }
 }
 
-public class TestThread {
+public class Ejemplo04Thread {
    public static void main(String args[]) {
 
       PrintDemo PD = new PrintDemo();
